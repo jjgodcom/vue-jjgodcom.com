eb7906ef27737643f73e414289daf03755c99001
jjgodcom <jjgodcom1@gmail.com>
2022-10-21 20:29:56 +0900

    프로젝트 생성

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Remove & use example component! -->
+  <Nav />
+  <RouterView />
+</template>
+
+<script>
+import Nav from '~/components/Nav'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
