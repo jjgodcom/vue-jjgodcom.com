4b6972afb7e98f4b10b446438fa28a0618d50d33
jjgodcom <jjgodcom1@gmail.com>
2022-11-08 09:23:10 +0900

    Header 중간 업데이트

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <Test />
+  <!-- 페이지별 확인할떄 쓰는거임 -->
+  <!-- <Test /> -->
+  <Header></Header>
   <RouterView />
+  <Footer></Footer>
 </template>
 
 <script>
 import Test from '~/components/Test'
+import Header from '~/components/common/Header'
+import Footer from '~/components/common/Footer'
 
 export default {
   components: {
-    Test
+    Test,
+    Header,
+    Footer
   }
 }
 </script>
