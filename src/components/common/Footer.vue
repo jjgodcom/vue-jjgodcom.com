684bced60e21aa37310b7ccb6e9caabdf18d05e2
jjgodcom <jjgodcom1@gamil.com>
2022-11-08 15:35:16 +0900

    폰트 테스트

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,5 +1,27 @@
 <template>
   <footer>
-    푸터
+    <p>🐢 Copyright {{year}}. Web Front-end developer Suho-Park 🐢</p>
   </footer>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      year: new Date().getFullYear()
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+footer {
+  width: 100%;
+  padding: 20px 0;
+  color: #292929;
+  font-size: 14px;
+  text-align: center;
+  border-top: 1px solid #ccc;
+}
+</style>
