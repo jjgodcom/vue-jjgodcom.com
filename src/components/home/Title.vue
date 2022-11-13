5556556ed406031b7f1cb38f6a4ee7d272aa883f
jjgodcom <jjgodcom1@gmail.com>
2022-11-13 22:32:08 +0900

    메인 타이틀

--- src/components/home/Title.vue
+++ src/components/home/Title.vue
@@ -3,7 +3,8 @@
     <h1>
       A Welcome to JJGODCOM world! 🖥️<br>
       Finding joy in front-end development! <br>
-      메인 맛이 없는데 다시 찾아서 만들어보자구나
+      : vue.js / javascript / scss / html <br>
+      나중에 타이핑 효과 넣기
     </h1>
   </div>
 </template>
@@ -11,11 +12,25 @@
 <style lang="scss" scoped>
 @import '~/scss/var.scss';
 
+.container {
+  padding: 100px 0;
+}
 h1 {
   font-family: $font--NotoSans;
   font-weight: bold;
   color:#191919;
   font-size: 3em;
   line-height: 1.43;
+}
+@keyframes typing {
+    from { width: 100% }
+    to { width: 0 }
+}
+
+
+@media (max-width: 1204px){
+  h1 {
+    font-size: 1.75em;
+  }
 }
 </style>
