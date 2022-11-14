e022cc581f3e4ad42cad935b5de40ee3d3181d56
jjgodcom <jjgodcom1@gmail.com>
2022-11-14 16:53:21 +0900

    about 영화페이지 링크 추가;

---
+++ src/components/Career.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h2>Carrea</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding-top: 100px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>About~</h1>
+  <a href="https://jjgodcom-vue-moviesearch.netlify.app/#/" target="_blank">영화</a>
 </template>
