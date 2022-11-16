9958a3fbab90615771515e0b274bb0eb8826ffde
jjgodcom <jjgodcom1@gmail.com>
2022-11-16 19:21:29 +0900

    test 페이지 전환효과 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <!-- 페이지별 확인할떄 쓰는거임 -->
-  <!-- <Test /> -->
   <Header></Header>
   <RouterView />
   <Footer></Footer>
 </template>
 
 <script>
-import Test from '~/components/Test'
 import Header from '~/components/common/Header'
 import Footer from '~/components/common/Footer'
 
 export default {
   components: {
-    Test,
     Header,
     Footer
   }
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,3 +1,35 @@
 <template>
   <h1>테스트전용페이지</h1>
+  <button v-on:click="show = !show">
+    Toggle
+  </button>
+  <transition name="slide-fade" mode="out-in">
+    <p v-if="show">hello</p>
+  </transition>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      show: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0.4;
+}
+</style>
