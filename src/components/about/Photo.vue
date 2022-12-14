bcb94fe53898825aa5bf136ff624201672a47fcf
jjgodcom <jjgodcom1@gmail.com>
2022-12-14 13:45:23 +0900

    캐릭터 교체

--- src/components/about/Photo.vue
+++ src/components/about/Photo.vue
@@ -3,14 +3,13 @@
   <div class="photo">
     <vue-load-image>
       <template v-slot:image>
-        <img src="../../assets/common/logo.png" />
+        <img src="../../assets/common/character.png" />
       </template>
       <template v-slot:preloader> 
         <img src="https://raw.githubusercontent.com/jjgodcom/vue-jjgodcom.com/f11e79fd131f723fa828cf254f1336a7458d9cb0/src/assets/common/loading.gif" />
       </template>
       <template v-slot:error> image load fails </template>
     </vue-load-image>
-    <p>이미지는 조금더 찾아보고 적용해보자</p>
   </div>
   <!-- /photo -->
 </template>
@@ -28,17 +27,10 @@ export default {
 <style lang="scss" scoped>
 @import '~/scss/var.scss';
 .photo{
-  width: 250px;
-  height: 250px;
-  margin: 40px auto 20px;
-  padding: 30px;
-  border: 10px solid #dee2e6;
-  border-radius: 50%;
-  box-sizing: border-box;
-  background-color: #e9ecef;
-  position: relative;
   img{
-    width: 100%;
+    width: 300px;
+    display: block;
+    margin: 0 auto;
   }
 }
 @include media890 {
