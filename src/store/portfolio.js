00b0fa2ea9c6a922f8bcf56ecd8e1c45b0297f02
jjgodcom <jjgodcom1@gmail.com>
2023-01-31 11:28:35 +0900

    모든 포트폴리오 업로드 완료

--- src/store/portfolio.js
+++ src/store/portfolio.js
@@ -106,12 +106,140 @@ export default {
           '사용자 페이지(HTML) 개발 (100%)'
         ],
         description:'아이디어 등록, 변리사 매칭, 특허 출원 신청까지 한번에 해결해주는 특허출원 도움 사이트 입니다.',
-        image:require('assets/portfolio/deaman01.png'),
         images:
         [
           require('assets/portfolio/deaman01.png').default,
           require('assets/portfolio/deaman02.png').default,
           require('assets/portfolio/deaman03.png').default
+        ]
+      },
+      {
+        title:'마크맨',
+        link:'',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'상표 등록, 상호 등록, 변리사 매칭, 상표 검색 등 상표와 관련된 서비스를 제공하는 사이트 입니다.',
+        images:
+        [
+          require('assets/portfolio/markman.png').default
+        ]
+      },
+      {
+        title:'문라이트',
+        link:'',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'흑백만화 스캔본을 자동으로 영상화 시켜주는 제작툴로 스캔본을 업로드만 하면 자동으로 컷을 분리, 자동 채색, 영상화 진행, 세부적인 연출 제작툴 을 제공 합니다.',
+        images:
+        [
+          require('assets/portfolio/moon01.png').default,
+          require('assets/portfolio/moon02.png').default,
+          require('assets/portfolio/moon03.png').default,
+          require('assets/portfolio/moon04.png').default
+        ]
+      },
+      {
+        title:'스팅',
+        link:'',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'스타 마케팅 플래폼(STING)은 스타트업이나 중소기업이 상품을 스타와 매칭하여 온라인으로 홍보를 할 수 있도록 하는 서비스입니다.',
+        images:
+        [
+          require('assets/portfolio/sting.png').default
+        ]
+      },
+      {
+        title:'자인맨',
+        link:'',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'디자인 출원을 누구든 손쉽고 간편하게 제공하는 사이트 입니다.',
+        images:
+        [
+          require('assets/portfolio/singman.png').default
+        ]
+      },
+      {
+        title:'투니비 라이트',
+        link:'',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'투니비 저작툴을 보다 간편하게 모바일로 웹툰을 업로드 하여 컷 편집, 영상 편집을 제공합니다.',
+        images:
+        [
+          require('assets/portfolio/lite.png').default
+        ]
+      },
+      {
+        title:'투니비 저작툴',
+        link:'https://toonivie.com/',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'투니비 저작툴은 웹툰 혹은 만화 단행본을 손쉽게 웹툰무비로 만드는 저작 플랫폼입니다. ',
+        images:
+        [
+          require('assets/portfolio/tool.png').default
+        ]
+      },
+      {
+        title:'투니비 플랫폼',
+        link:'https://toonivie.com/',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'투니비(Toonivie) 플랫폼은 웹툰무비, 웹툰, 단편을 업로드하는 플랫폼 입니다.',
+        images:
+        [
+          require('assets/portfolio/toonivie.png').default
+        ]
+      },
+      {
+        title:'투니비 플랫폼 2019',
+        link:'https://toonivie.com/',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'투니비(Toonivie) 플랫폼은 웹툰무비, 웹툰, 단편을 업로드하는 플랫폼 입니다.',
+        images:
+        [
+          require('assets/portfolio/toonivie2019.png').default
+        ]
+      },
+      {
+        title:'키즈페이',
+        link:'',
+        language:'HTML, CSS, Javascript, jQuery, MySQL, Spring',
+        works:
+        [
+          '사용자 페이지(HTML) 개발 (100%)'
+        ],
+        description:'아토큐브 (attocube) 에서 외주를 받아 진행한 유아도구 결제, 주문, 관리를 간편하게 도와주는 사이트 입니다.',
+        images:
+        [
+          require('assets/portfolio/kids.png').default
         ]
       },
 
